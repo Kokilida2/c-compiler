35ab799532f1b6a63dbb4f1366a2cc0cdf70d025
Kokilida <lior.shaked.8@gmail.com>
2021-03-03 11:58:36 +0200

    trying to fix something bad I did lol

--- assembler.h
+++ assembler.h
@@ -4,16 +4,30 @@
 
 enum label_state {DATA, CODE, EXTERN, ENTRY}
 
+struct word 
+{
+  unsigned int value:12;
+};
+
 struct label{
   char* name;
   enum label_state state;
-  word address;
+  struct word address;
 };
 
-struct word 
-{
-  int value:12;
+struct opWord {
+    char dest : 2;
+    char src : 2;
+    char func : 4;
+    char opCode : 4;
 };
+
+union convert {
+    struct opWord a;
+    struct word b;
+};
+
+
 
 
 
